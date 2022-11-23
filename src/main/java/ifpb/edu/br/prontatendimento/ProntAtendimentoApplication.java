50cfb0adbb0f662839c6db1e1a08f4ecd89947e6
Joyce Pinheiro <joyce.real01@gmail.com>
2022-11-23 15:37:57 -0300

    Revert "Adicionando Spring Security e criação da classe Usuário, encriptar senha."
    This reverts commit 62c74232787f24295d02b3a48cb36a386121ffc0.

--- src/main/java/ifpb/edu/br/prontatendimento/ProntAtendimentoApplication.java
+++ src/main/java/ifpb/edu/br/prontatendimento/ProntAtendimentoApplication.java
@@ -2,23 +2,12 @@ package ifpb.edu.br.prontatendimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class ProntAtendimentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProntAtendimentoApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder(){
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
 	}
 
 }
