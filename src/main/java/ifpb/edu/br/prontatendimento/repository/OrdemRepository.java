fdd5069308181a0c7344730156230a0a14889596
Joyce Pinheiro <joyce.real01@gmail.com>
2022-11-23 15:42:34 -0300

    Revert "Adicionando classe Ordem e adição de JWT e autenticação do Usuário"
    This reverts commit 612b4c763209444128ca0ec037931fd95f7d2fa7.

--- src/main/java/ifpb/edu/br/prontatendimento/repository/OrdemRepository.java
+++
@@ -1,7 +0,0 @@
-package ifpb.edu.br.prontatendimento.repository;
-
-import ifpb.edu.br.prontatendimento.model.Ordem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdemRepository extends JpaRepository<Ordem, Integer> {
-}
